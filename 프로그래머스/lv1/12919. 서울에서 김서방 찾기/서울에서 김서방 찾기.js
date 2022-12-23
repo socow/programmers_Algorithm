c4a158060b33ea91b6d4600d552f255a710f93c8
so jaehyun <105201721+socow@users.noreply.github.com>
2022-12-23 15:54:08 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,9 +1,5 @@
 function solution(seoul) {
-    var answer = '';
-    for(let i = 0; i <seoul.length; i++){
-        if(seoul[i] == 'Kim'){
-            answer = `김서방은 ${i}에 있다`
-        }
-    }
-    return answer;
+    let kim1 = seoul.indexOf("Kim");
+    var answer = '김서방은 '+`${kim1}`+'에 있다';
+    return answer
 }
