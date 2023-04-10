e467ae9ff325efd3dec22644842234ab01e5bb35
so jaehyun <105201721+socow@users.noreply.github.com>
2023-04-10 21:24:13 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,10 +1,13 @@
 function solution(n) {
-    let arr = new Set();
-    for(let i = 1; i <= n; i++){
+    var answer = 0;
+    for(let i = 4; i <= n; i++){
         for(let j = 2; j < i; j++){
-            if(i % j === 0)
-                arr.add(i);
+            if(i % j === 0){
+                answer += 1;
+                break;
+            }
         }
     }
-    return arr.size;
+
+    return answer;
 }
