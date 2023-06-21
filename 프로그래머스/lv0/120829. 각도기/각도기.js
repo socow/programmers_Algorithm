4d1925ce218e623199f3b617840f88c706474cd2
so jaehyun <105201721+socow@users.noreply.github.com>
2023-06-21 14:57:07 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,6 +1,12 @@
 function solution(angle) {
-  if (angle === 180) return 4;
-  if (angle > 90) return 3;
-  if (angle === 90) return 2;
-  return 1;
+    if(angle<90){
+        return 1
+    }else if(angle === 90){
+        return 2
+    }else if(angle === 180){
+        return 4
+    }else{
+        return 3
+    }
+
 }
